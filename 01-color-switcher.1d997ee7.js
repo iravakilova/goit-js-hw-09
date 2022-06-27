cb6803aabd14067788b7e34f12043467340cc252
iravakilova <52080853+iravakilova@users.noreply.github.com>
2022-06-27 16:59:10 +0000

    Deploying to gh-pages from @ iravakilova/goit-js-hw-09@20508456e5b0e091661480d10f4043d716b24e04 🚀

---
+++ 01-color-switcher.1d997ee7.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let r=null;t.addEventListener("click",(()=>{r=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.setAttribute("disabled",!0),e.removeAttribute("disabled",!1)}),1e3)})),e.addEventListener("click",(()=>{clearInterval(r),t.removeAttribute("disabled",!1),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.1d997ee7.js.map
