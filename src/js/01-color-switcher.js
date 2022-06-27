20508456e5b0e091661480d10f4043d716b24e04
Iryna Vakilova <iravakilova@gmail.com>
2022-06-27 19:58:39 +0300

    task-1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timer = null;
+startBtn.addEventListener("click", () => {
+  timer = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+      startBtn.setAttribute("disabled", true); 
+      stopBtn.removeAttribute("disabled", false);
+  }, 1000);
+});
+stopBtn.addEventListener("click", () => {
+    clearInterval(timer);
+    startBtn.removeAttribute("disabled", false);
+    stopBtn.setAttribute("disabled", true);
+});
 
