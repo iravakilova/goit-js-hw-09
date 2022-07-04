2630a19deb098ff6a84cf15a60de8a05f251e306
iravakilova <52080853+iravakilova@users.noreply.github.com>
2022-07-04 13:50:19 +0000

    Deploying to gh-pages from @ iravakilova/goit-js-hw-09@8e42d7623535f6bd788806794137d78a510c633a 🚀

--- 01-color-switcher.8e22436d.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),t.setAttribute("disabled",!0),e.removeAttribute("disabled",!1)}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled",!1),e.setAttribute("disabled",!0)}))}();
-//# sourceMappingURL=01-color-switcher.8e22436d.js.map
